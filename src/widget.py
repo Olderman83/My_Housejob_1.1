0c4d55df35a75ba97149815c06df7d76e294e75c
Павел Руцкин <Pavel-ra83@yandex.ru>
2025-11-16 12:20:32 +0400

    save widget2

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
-from .masks import get_mask_card_number, get_mask_account
+from .masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_info:str) -> str:
+def mask_account_card(card_info: str) -> str:
     """Принимает данные карты, возвращает строку с замаскированным номером"""
 
     card_info_part = card_info.split()
@@ -18,16 +18,16 @@ def mask_account_card(card_info:str) -> str:
         return f"{card_name} {masked}"
 
 
-def get_date(date:str) -> str:
+def get_date(date: str) -> str:
     """Принимает строку с датой в формате
-"2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
-"ДД.ММ.ГГГГ" """
+    "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
+    "ДД.ММ.ГГГГ" """
 
     split_date = date.split("T")
     date_part = split_date[0]
 
-    year = date_part[0: 4]
-    month = date_part[5: 7]
-    day = date_part[8: 10]
+    year = date_part[0:4]
+    month = date_part[5:7]
+    day = date_part[8:10]
 
     return f"{day}.{month}.{year}"
