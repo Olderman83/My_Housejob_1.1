23884ff23037f29b8e6454a822c7723c017f97b7
Павел Руцкин <pavelru163@gmail.com>
2025-11-23 16:09:50 +0400

    new_processing2

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,10 @@ from typing import List, Dict, Any
 from datetime import datetime
 
 def filter_by_state(transactions, state = "EXECUTED") -> list:
-    """Фильтрует список словарей по значению ключа "state" """
+    """ Функция принимает список словарей и опционально значение для ключа "state" (по умолчанию
+'EXECUTED'). Возвращает новый список словарей, содержащий только те словари, у которых ключ
+"state" соответствует указанному значению """
+
     filtered_list = []
     for transaction in transactions:
         if transaction.get("state") == state:
@@ -10,7 +13,8 @@ def filter_by_state(transactions, state = "EXECUTED") -> list:
     return filtered_list
 
 
-
-
-
+def sort_by_date(dict_list:List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Возвращает новый список, отсортированный по дате"""
+    return sorted(dict_list, key = lambda x: x["date"], reverse = reverse)
 
